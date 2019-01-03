1a643e01b9e540897a68c9fe7ee7e519a28373ea
zhuxiaopan <751554172@qq.com>
2019-01-03 20:16:28 +0800

    本地 工程添加到git

---
+++ firstClass/src/cn/itcast/FirstClass.java
@@ -0,0 +1,23 @@
+package cn.itcast;
+
+/**
+ * @Author: 朱晓攀
+ * @Date: 2019/01/03
+ * @Description: cn.itcast
+ *
+ * github推送和拉取测试
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
